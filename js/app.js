8a4720d69b37ab8666178579bcce884610268bd6
niwong <nick@nickwong.io>
2018-11-17 19:47:41 -0800

    mo boba time functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
+// play button
 document.querySelector('#fireButton').addEventListener('click', function() {
 	var flames = document.getElementById('flames')
 	flames.play();
 	console.log('bitch im a cow')
 });
 
+// stop button
 document.querySelector('#putOutFire').addEventListener('click', function() {
 	flames.pause();
 	console.log('no can handle')
 });
+
+// time test
+var time = new Date();
+min = time.getMinutes();
+hour = time.getHours();
+
+if (hour == 3 || hour == 15 && min == 11) {
+	flames.play();
+}
